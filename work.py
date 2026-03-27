e4b7555c45245f08151cd3f9412b2c6e53f2d41f
dimpdimp <271378542+dimpdimp@users.noreply.github.com>
2026-03-26 20:30:31 -0400

    Create work.py
    Added my first money goal script

---
+++ work.py
@@ -0,0 +1,5 @@
+goal=int(input("How much money do you want to make? 1000$"))
+days=124
+daily_grind=goals/days
+print(f"To hit ${goal}by July 29, you need to earn ${daily_grind:.2f}per day.")
+print("The clock is ticking. Get to work.")
